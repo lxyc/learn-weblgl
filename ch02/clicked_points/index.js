4789d9de67cafc0bb2f007f0ea8ce95d0b111969
xiongyucheng <xiongyucheng@megvii.com>
2021-06-22 18:02:29 +0800

    add: 点击绘制

--- ch02/clicked_points/index.js
+++ ch02/clicked_points/index.js
@@ -1,9 +1,10 @@
 // 顶点着色程序
 const VSHADER_SOURCE = `
   attribute vec4 a_Position;
+  attribute float a_PointSize;
   void main() {
     gl_Position = a_Position;
-    gl_PointSize = 10.0;
+    gl_PointSize = a_PointSize;
   }
 `;
 
@@ -25,53 +26,44 @@ ready(() => {
   }
 
   const a_Position = gl.getAttribLocation(gl.program, "a_Position");
-  if (a_Position < 0) {
+  const a_PointSize = gl.getAttribLocation(gl.program, "a_PointSize");
+  if (a_Position < 0 || a_PointSize < 0) {
     console.error("Failed to get the storage attribute location!");
     return;
   }
-
+  
   const u_FragColor = gl.getUniformLocation(gl.program, "u_FragColor");
   if (!u_FragColor) {
     console.error("Failed to get the storage uniform location");
     return;
   }
 
-  canvas.onmousedown = (evt) => {
-    click(evt, canvas, gl, a_Position, u_FragColor);
-  };
+  gl.vertexAttrib1f(a_PointSize, 10.0);
+  gl.uniform4f(u_FragColor, 1.0, 0.0, 0.0, 1.0);
+
+  canvas.addEventListener('click', (evt) => {
+    handleClick(evt, gl, a_Position);
+  });
 
   // 清空颜色缓冲区
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 });
 
+
 const points = [];
-const colors = [];
-const click = (evt, canvas, gl, pLocation, fLocation) => {
-  let { clientX: x, clientY: y } = evt;
+const handleClick = (evt, gl, pLocation) => {
+  const { clientX: x, clientY: y } = evt;
   const rect = evt.target.getBoundingClientRect();
 
-  x = (x - rect.left) / (canvas.width / 2) - 1;
-  y = 1 - (y - rect.top) / (canvas.height / 2);
-  points.push({ x, y });
-
-  let pColor;
-  if (x > 0) {
-    pColor = y > 0 ? [1.0, 0.0, 0.0, 1.0] : [0.0, 1.0, 0.0, 1.0];
-  } else {
-    pColor = y > 0 ? [0.0, 0.0, 1.0, 1.0] : [1.0, 1.0, 1.0, 1.0];
-  }
-  colors.push(pColor);
-
-  // 绘制前先用指定颜色清空缓冲区。否则默认 clear() 黑色全透明背景色
+  const pointX = (x - rect.left) / (rect.width / 2) - 1;
+  const pointY = 1 - (y - rect.top) / (rect.height / 2);
+  points.push([pointX, pointY]);
+  
   gl.clear(gl.COLOR_BUFFER_BIT);
-
-  points.forEach((_, index) => {
-    const point = points[index];
-    const color = colors[index];
-
-    gl.vertexAttrib3f(pLocation, point.x, point.y, 0.0);
-    gl.uniform4f(fLocation, ...color);
-    gl.drawArrays(webgl.POINTS, 0, 1);
+  
+  points.forEach(([x, y]) => {
+    gl.vertexAttrib3f(pLocation, x, y, 0.0);
+    gl.drawArrays(gl.POINTS, 0, 1);
   });
 };
